cd8c1ea04f19a0f3f4aebe6cf7bed5bab0bb86ec
Elisa <elisahonoratolira@uc.cl>
2024-02-06 11:13:48 -0300

    upfate

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
-module.exports = {
-    output: 'export',
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output:'export',
   };
+  /* module.exports = nextConfig*/
+  export default nextConfig;
+  
