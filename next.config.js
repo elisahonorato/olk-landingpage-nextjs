a22b6c59dcf39b1bda52370d3bd16a41b924cd79
Elisa <elisahonoratolira@uc.cl>
2024-02-06 11:30:06 -0300

    next export

--- next.config.js
+++ next.config.js
@@ -2,18 +2,7 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-
-    // Optional: Set this to true if you plan to do static export
-    
-   
-    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    // trailingSlash: true,
-   
-    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-    // skipTrailingSlashRedirect: true,
-   
-    // Optional: Change the output directory `out` -> `dist`
-    // distDir: 'dist',
-  }
-   
-  module.exports = nextConfig
+    trailingSlash: true,
+    output: 'export',
+    }
+    module.exports = nextConfig
