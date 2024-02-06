205653966e2bf53b635d847d81a9ec0617599b17
Elisa <elisahonoratolira@uc.cl>
2024-02-06 17:53:30 -0300

    better gdsp

--- src/app/components/ScrollSection.tsx
+++ src/app/components/ScrollSection.tsx
@@ -39,11 +39,11 @@ function ScrollSection() {
           gsap.fromTo(
             textRef.current,
             { opacity: 0, y: 50 },
-            { opacity: 1, y: 0, duration: 1, delay: index * 0.5, ease: "power3.out" } // Ajustar el retraso según el índice
+            { opacity: 1, y: 0, duration: 1, delay: index * 0.5 } // Ajustar el retraso según el índice
           );
         },
         onLeaveBack: () => {
-          gsap.to(textRef.current, { opacity: 0, y: 50, duration: 1, ease: "power3.in" });
+          gsap.to(textRef.current, { opacity: 0, y: 50, duration: 1 });
         }
       });
     });
@@ -60,18 +60,27 @@ function ScrollSection() {
           <div className="page">
             <div className="flex flex-col justify-between ml-10">
               <h3 className="mt-10 font-bold text-lg">Automation of project control</h3>
-              <div className="flex flex-row mt-8 gap-5">
+              <div className="flex flex-row mt-10 gap-5">
                 <div className="rounded-lg shadow-lg bg-primary-light p-4" style={{ width: "50%" }}>
                   <video autoPlay muted loop width="100%" height="auto" className="rounded-t-lg cover">
                     <source src="./videos/screen/automated-progress.mp4" type="video/mp4" />
                   </video>
                 </div>
                 <div className="w-1/2">
-                  <h2 className="mb-4 font-semibold text-gray-900 dark:text-white text-lg">Automatic measurement of construction progress, as well as planning and scheduling</h2>
-                  <ul className="space-y-2 text-gray-500 list-disc list-inside dark:text-gray-400">
-                    <li>Without workers intervention</li>
-                    <li>BIM enviroment</li>
-                    <li>Automated quantity calculation</li>
+                  <h2 className="mb-2 font-semibold text-gray-900 dark:text-white text-md">Automatic measurement of construction
+progress, as well as planning and scheduling</h2>
+                  <ul className="space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400 mt-10">
+                    <li>
+                    Without workers
+intervention
+                    </li>
+                    <li>
+                    BIM enviroment
+                    </li>
+                    <li>
+                    Automated quantity
+calculation
+                    </li>
                   </ul>
                 </div>
               </div>
@@ -81,18 +90,31 @@ function ScrollSection() {
           <div className="page">
             <div className="flex flex-col justify-between ml-10">
               <h3 className="mt-10 font-bold text-lg">Concrete strength measurements</h3>
-              <div className="flex flex-row mt-8 gap-5">
+              <div className="flex flex-row mt-10 gap-5">
                 <div className="rounded-lg shadow-lg bg-primary-light p-4" style={{ width: "50%" }}>
                   <video autoPlay muted loop width="100%" height="auto" className="rounded-t-lg cover">
                     <source src="./videos/screen/planification.mp4" type="video/mp4" />
                   </video>
                 </div>
                 <div className="w-1/2">
-                  <h2 className="mb-4 font-semibold text-gray-900 dark:text-white text-lg">Once concrete is detected, ObraLink measures the strength in real-time without the need for worker intervention</h2>
-                  <ul className="space-y-2 text-gray-500 list-disc list-inside dark:text-gray-400">
-                    <li>It accelerates construction processes</li>
-                    <li>Concrete strength every 20 mins</li>
-                    <li>Data displayed in a BIM context</li>
+                  <h2 className="mb-2 font-semibold text-gray-900 dark:text-white text-md">Once concrete is detected, ObraLink
+                    measures the strength in real-time without
+                    the need for worker intervention</h2>
+                  <ul className="space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400 mt-10">
+                    <li>
+                      It accelerates
+                      construction
+                      processes
+                    </li>
+                    <li>
+                      Concrete
+                      strength every
+                      20 mins
+                    </li>
+                    <li>
+                      Data displayed in
+                      a BIM context
+                    </li>
                   </ul>
                 </div>
               </div>
@@ -102,18 +124,29 @@ function ScrollSection() {
           <div className="page">
             <div className="flex flex-col justify-between ml-10">
               <h1 className="mt-10 font-bold text-lg">Concrete strength measurements</h1>
-              <div className="flex flex-row mt-8 gap-5">
+              <div className="flex flex-row mt-10 gap-5">
                 <div className="rounded-lg shadow-lg bg-primary-light p-4" style={{ width: "50%" }}>
                   <video autoPlay muted loop width="100%" height="auto" className="rounded-t-lg cover">
                     <source src="./videos/screen/planification.mp4" type="video/mp4" />
                   </video>
                 </div>
                 <div className="w-1/2">
-                  <h2 className="mb-4 font-semibold text-gray-900 dark:text-white text-lg">Key project data is automatically shared with concrete providers to enhance oversight and optimize concrete delivery</h2>
-                  <ul className="space-y-2 text-gray-500 list-disc list-inside dark:text-gray-400">
-                    <li>Live Concrete Pouring Updates</li>
-                    <li>Continuous Monitoring of Concrete Strength and Behavior</li>
-                    <li>Predictive Analysis for Concrete Deliveries</li>
+                  <h2 className="mb-2 font-semibold text-gray-900 dark:text-white text-md">Key project data is automatically shared
+                    with concrete providers to enhance
+                    oversight and optimize concrete delivery</h2>
+                  <ul className="space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400 mt-10">
+                    <li>
+                      Live Concrete
+                      Pouring Updates
+                    </li>
+                    <li>
+                      Continuous Monitoring of
+                      Concrete Strength and Behavior
+                    </li>
+                    <li>
+                      Predictive Analysis for
+                      Concrete Deliveries
+                    </li>
                   </ul>
                 </div>
               </div>
