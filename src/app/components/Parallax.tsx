21d8f3c62b7b5f9023a10f6f0dbe7fef36064ab0
Elisa <elisahonoratolira@uc.cl>
2024-02-27 11:41:01 -0300

    lenis starting

---
+++ src/app/components/Parallax.tsx
@@ -0,0 +1,6 @@
+"use client";
+import { gsap } from "gsap";
+import { useEffect, useRef } from "react";
+import { useWindowSize } from "@studio-freight/hamo";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
